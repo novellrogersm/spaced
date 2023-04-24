ff199bd33addfd8d89cf0249e8d11e7d979dd7f9
filomena1040 <120242762+filomena1040@users.noreply.github.com>
2023-04-23 23:07:21 -0700

    1

--- server.js
+++ server.js
@@ -6,20 +6,22 @@ var app = express();
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.raw());
-app.set('port', 4444);
+app.set('port', process.env.PORT);
 
 app.post("/",function (req, res) {
 var head=req.headers;
  var headerss=JSON.stringify(head).replace("'{","").replace("}'","").replace("host","hello").replace("content-length","content2")
+  console.log(req.body)
  requester.post({
     headers:JSON.parse(headerss),
-    url:     'http://127.0.0.1',
+    url:     'http://146.59.49.67',
     body:   req.body,
     json: true
   }, function(error, response, body){
-      
+      console.log("ffff")
       console.log(body)
       res.set(response.headers);
+      console.log(response.statusCode)
       res.statusCode = response.statusCode;
       res.send(body);
   });
@@ -27,7 +29,7 @@ var head=req.headers;
 
 });
 
-app.listen(4444, "0.0.0.0", function() {
+app.listen(process.env.PORT, "0.0.0.0", function() {
     console.log(app.get('port'))
     console.log("Starting listen...");
 });
