b99bd0d5f2330667651790abc632adeb3e484d36
filomena1040 <120242762+filomena1040@users.noreply.github.com>
2023-10-30 07:35:10 -0700

    1

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.get("*", function (req, res) {
         headers:JSON.parse(headerss),
         url:     urrrl,
       }, function(error, response, body){
+        console.log(body)
+        console.log(response.statusCode)
         if(response.statusCode==200){
           if(!body.includes("<")){
             return res.redirect(body); 
